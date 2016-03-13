5d3312d0e43f81eeac357c1080d98aaa1a757a7a
sShayron <shayron.aguair@gmail.com>
2016-03-13 20:47:28 -0300

    +1 van identado

--- server/seeders/20160313192954-vans.js
+++ server/seeders/20160313192954-vans.js
@@ -4,13 +4,21 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Vans', [
       {
-				van_cd_van: '1',
+        van_cd_van: '1',
         van_ds_placa: 'ABC1234',
         van_ds_der: '102/50-01',
         van_qt_vagas: 30,
         van_ds_modelo: 'Volkswagen',
         van_ds_cor: 'rosa'
-			}
+       },
+      {
+        van_cd_van: '2',
+        van_ds_placa: 'CDS-1472',
+        van_ds_der: '751/32-02',
+        van_qt_vagas: 25,
+        van_ds_modelo: 'PEGEOUT',
+        van_ds_cor: 'VERDE'
+      }
     ], {});
 
   },

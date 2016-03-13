d41cfadc49a630bc89bc9a695f5a1e6e76382c6e
Armando Magalhães <armando.mag95@gmail.com>
2016-03-13 20:48:26 -0300

    linha 13

--- server/seeders/20160313192954-vans.js
+++ server/seeders/20160313192954-vans.js
@@ -10,7 +10,7 @@ module.exports = {
         van_qt_vagas: 30,
         van_ds_modelo: 'Volkswagen',
         van_ds_cor: 'rosa'
-       },
+      },
       {
         van_cd_van: '2',
         van_ds_placa: 'CDS-1472',
@@ -20,7 +20,6 @@ module.exports = {
         van_ds_cor: 'VERDE'
       }
     ], {});
-
   },
 
   down: function (queryInterface, Sequelize) {

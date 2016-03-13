434fffcbc153075a53bc213f261a1a96ddef096b
Armando Magalhães <armando.mag95@gmail.com>
2016-03-13 16:07:26 -0300

    testes

--- server/controllers/bairro.js
+++ server/controllers/bairro.js
@@ -5,13 +5,17 @@ module.exports = function (app) {
 	var controller = {};
 
 	controller.find = function(req, res) {
-    models.Bairro.findById(req.params.id).then(function(bairro) {
+
+		models.Bairro.findById(req.params.id).then(function(bairro) {
+			res.type('json');
       res.json(bairro);
     });
 	};
 
 	controller.findAll = function(req, res) {
+
     models.Bairro.findAll().then(function(bairros) {
+			res.type('json');
       res.json(bairros);
     });
 	};
---
+++ server/test/controllers/bairro.js
@@ -0,0 +1,41 @@
+const app = require('../../config/app');
+const request = require('supertest');
+const expect = require('chai').expect;
+
+var arrayBairros = [];
+
+describe("GET /bairro", function() {
+	it("deve retornar um json array", function(done) {
+		request(app)
+		.get('/bairro')
+		.expect('Content-Type', /json/)
+		.expect(200)
+		.end(function (err, res) {
+			if (err)
+				throw err;
+
+			expect(res.body).to.be.an('array');
+
+			arrayBairros = res.body;
+
+			done();
+		});
+	});
+});
+
+describe("GET /bairro/:id", function() {
+	it("deve retornar um json object", function(done) {
+		request(app)
+		.get('/bairro/' + arrayBairros[0].id)
+		.expect('Content-Type', /json/)
+		.expect(200)
+		.end(function (err, res) {
+			if (err)
+				throw err;
+
+			expect(res.body).to.be.a('object');
+
+			done();
+		});
+	});
+});

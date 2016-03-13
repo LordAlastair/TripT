6fab5a9f26b3276638e2951621efc6bee06d5a3d
Armando Magalhães <armando.mag95@gmail.com>
2016-03-13 20:28:20 -0300

    indentacao

--- server/seeders/20160313192954-vans.js
+++ server/seeders/20160313192954-vans.js
@@ -3,12 +3,14 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Vans', [
-      { van_cd_van: '1',
+      {
+				van_cd_van: '1',
         van_ds_placa: 'ABC1234',
         van_ds_der: '102/50-01',
         van_qt_vagas: 30,
         van_ds_modelo: 'Volkswagen',
-        van_ds_cor: 'rosa' }
+        van_ds_cor: 'rosa'
+			}
     ], {});
 
   },

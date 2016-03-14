c22c33f8012761e3c05d985f0b006efb7431379a
Armando Magalhães <armando.mag95@gmail.com>
2016-03-14 20:37:29 -0300

    implementacao do metodo controller/van#create()

--- server/controllers/van.js
+++ server/controllers/van.js
@@ -16,5 +16,19 @@ module.exports = function (app) {
     });
   };
 
+  controller.create = function(req, res) {
+    // TEST: curl -v -X POST http://localhost:3000/van -d '{ "van_ds_placa": "ABC1234", "van_ds_der": "102/50-01", "van_qt_vagas": 30, "van_ds_modelo": "Volkswagen", "van_ds_cor": "rosa"}' -H "Content-Type: application/json"
+
+    models
+    .Van
+    .create(req.body)
+    .then(function(van) {
+      res.json(van);
+    })
+    .catch(function(error) {
+      res.status(500).json(error);
+    });
+  };
+
   return controller;
 };

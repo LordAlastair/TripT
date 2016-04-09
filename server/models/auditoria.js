7529f694bef93b70512a38ea3e341dddf8c25b61
Carlos Gatti <eduardo.gf@hotmail.com>
2016-04-09 16:10:41 -0300

    Model tabela auditoria

---
+++ server/models/auditoria.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Sequelize = sequelize.Sequelize;
+
+  var Auditoria = sequelize.define('Auditoria', {
+    aud_cd_auditoria: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    aud_ds_tabela: {
+      allowNull: false,
+      type: Sequelize.STRING(45)
+    },
+    aud_ds_alteracao: {
+      allowNull: false,
+      type: Sequelize.STRING(45)
+    },
+    
+    aud_cd_usuario: {
+      allowNull: false,
+      type: Sequelize.STRING(45)
+    },
+    aud_ts_modificacao: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    aud_ds_modificacao: {
+      allowNull: false,
+      type: Sequelize.STRING(45)
+    }
+  }, {
+    timestamps: false,
+    classMethods: {
+      associate: function(models) {
+      }
+    }
+  });
+  return Auditoria;
+};

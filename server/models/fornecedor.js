d11274703b116ecda8d3e34d0613b77cb095ab05
Johnson Antunes <johnsonantuness@gmail.com>
2016-04-10 16:10:00 -0300

    Implementado back-end do cadastro de Fornecedor.

---
+++ server/models/fornecedor.js
@@ -0,0 +1,86 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes){
+  var Sequelize = sequelize.Sequelize;
+
+  var Fornecedor = sequelize.define('Fornecedor', {
+    for_cd_fornecedor: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+
+    for_ds_pessoa: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+
+    for_ds_razao_social: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+
+    for_ds_fantasia_nome: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+
+    for_ds_cnpj_cpf: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+
+    for_ds_cep: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+
+    for_ds_endereco: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+
+    for_ds_numero: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+
+    for_ds_completo: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+
+    for_ds_bairro: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+
+    for_ds_cidade: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+
+    for_ds_celular: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+
+    for_ds_email: {
+      allowNull: false,
+      type: Sequelize.STRING
+    }
+  }, {
+    timestamps: false,
+    classMethods: {
+      associate: function(models) {
+        // Fornecedor.hasOne(models.Usuario, {
+        //   foreignKey: 'usu_cd_usuario',
+        //   allowNull: false
+        // });
+      }
+    }
+  });
+
+  return Fornecedor;
+};

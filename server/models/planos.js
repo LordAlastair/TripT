1083b5637367a008d6ad6a19d3d4d9cccd7fcdc9
Shayron <shayron.aguiar@gmail.com>
2016-04-27 11:14:19 -0300

    Correcao

--- server/models/planos.js
+++ server/models/planos.js
@@ -20,7 +20,7 @@ module.exports = function(sequelize, DataTypes){
     classMethods: {
       associate: function(models) {
         Planos.hasMany(models.FornecedorPlano, {
-          foreignKey: 'for_cd_plano',
+          foreignKey: 'fop_cd_plano',
           allowNull: false
         });
       }

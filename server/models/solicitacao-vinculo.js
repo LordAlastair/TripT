9c6e9a81d49133c453e1c31a6ce66583e826c3b5
Carlos Gatti <eduardo.gf@hotmail.com>
2016-05-03 19:13:05 -0300

    Model solicitação vinculo, atualização diagrama.

---
+++ server/models/solicitacao-vinculo.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Sequelize = sequelize.Sequelize;
+  var SolicitacaoVinvulo = sequelize.define('SolicitacaoVinvulo', {
+    sov_cd_solicitacao_vinculo: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    sov_cd_cliente: {
+      allowNull: false,
+      type: Sequelize.INTEGER
+    },
+    sov_cd_plano_vinculo: {
+      allowNull: false,
+      type: Sequelize.INTEGER
+    },  
+    sov_bl_aprovacao: {
+      allowNull: false,
+      type: Sequelize.BOOLEAN
+    },
+  }, {
+    timestamps: false
+  });
+  return SolicitacaoVinvulo;
+};

5a8fa60b8d65e5471d4ecf62620f37033ceef715
Shayron Aguiar <shayron.aguair@gmail.com>
2016-05-12 16:36:25 -0300

    af

--- server/routes/fornecedor.js
+++ server/routes/fornecedor.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
   /** exige que tenha o header Authorization na requisição */
   router.use(passport.authenticate('jwt', { session: false }));
 
-  router.get('/:', controller.find);
+  router.get('/', controller.find);
   router.put('/', controller.update);
 
   router.post('/', controller.create);
